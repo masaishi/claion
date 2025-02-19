2850af5ded56e76dda0352982f73d73d9fdb3922
Masamune William Ishihara <mwishiha@ucsc.edu>
2025-02-18 23:35:53 -0800

    feat: make audios folder by using extract_audio

--- src/clarion_ai/data/speechocean762.py
+++ src/clarion_ai/data/speechocean762.py
@@ -1,16 +1,15 @@
 from datasets import load_dataset
 
-from clarion_ai.data.utils import get_root_path
+from clarion_ai.data.utils import extract_audio, get_root_path
 
 
 def download_speechocean762():
     save_path = get_root_path() / "data" / "speechocean762"
     save_path.mkdir(parents=True, exist_ok=True)
 
-    print("Downloading Speechocean762 dataset...")
     dataset = load_dataset("mispeech/speechocean762")
     dataset.save_to_disk(save_path)
-    print(f"Speechocean762 dataset saved in {save_path}")
+    extract_audio(save_path)
 
 
 if __name__ == "__main__":
